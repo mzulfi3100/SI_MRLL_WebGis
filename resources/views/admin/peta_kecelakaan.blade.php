9834e9cd8bb343224029284732cb3d21fed83cbe
mzulfi3100 <mzulfikar3100@gmail.com>
2023-01-16 10:03:48 +0700

    Memperbarui view peta kecelakaan

--- resources/views/admin/peta_kecelakaan.blade.php
+++ resources/views/admin/peta_kecelakaan.blade.php
@@ -4,27 +4,75 @@
 @stop
 @section('script_peta')
 <script>
-    var mymap = L.map("mapid").setView([ -5.390000, 105.292969], 12.4);
-
-    L.tileLayer(
-        'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-        {
+    var street = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
-        }
-    ).addTo(mymap);
+    });
 
-    var myStyle = {
+    var hybrid = L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    });
+
+    var satellite = L.tileLayer('http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    });
+
+    var kabupatenJson;
+
+    $.ajax({
+        url: "/kabupaten.geojson",
+        async: false,
+        dataType: 'json',
+        success: function(data){
+            kabupatenJson = data
+        }
+    });
+
+    var kabupatenStyle = {
         "color": '#000000',
         "weight": 2,
         "opacity": 1,
         "fillOpacity": 0 ,
     };
 
-    $.getJSON('/kabupaten.geojson', function(json){
-        L.geoJSON(json, {
-            style: myStyle
-        }).addTo(mymap);
+    var batasKabupaten = L.geoJSON(kabupatenJson, {
+        style: kabupatenStyle
     });
+
+    var batasKecamatan = L.layerGroup();
+
+    var myMap = L.map('mapid', {
+        center: [-5.420000, 105.292969],
+        zoom: 12.4,
+        layers: [street, hybrid, satellite, batasKabupaten, batasKecamatan]
+    });
+
+    var baseMaps = {
+        "Street": street,
+        "Hybrid": hybrid,
+        "Satellite": satellite,
+    };
+
+    var overlayMaps = {
+        "Kabupaten": batasKabupaten,
+        "Kecamatan": batasKecamatan
+    };
+
+    @foreach($kecamatans as $kec)
+        var kecamatanStyle = {
+            "color": "<?= $kec->warnaKecamatan ?>",
+            "weight": 0,
+            "opacity": 1,
+            "fillOpacity": 0.5 ,
+        };
+        L.geoJSON(<?= $kec->batasKecamatan ?>,{
+            style: kecamatanStyle    
+        }).addTo(batasKecamatan).bindPopup(" <?= $kec->namaKecamatan ?> ");
+    @endforeach
+    
+    var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(myMap);  
+
 </script>
 @stop
