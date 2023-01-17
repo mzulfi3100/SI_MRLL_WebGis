74ee1c683f2dc542fffb62cb3e3b284097339243
mzulfi3100 <mzulfikar3100@gmail.com>
2023-01-17 09:30:53 +0700

    Add Dropdown Administrasi Kecamatan pada Peta

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -15,7 +15,6 @@
 <!-- ./wrapper -->
 
 <!-- REQUIRED SCRIPTS -->
-@yield('script_peta')
 <!-- jQuery -->
 <script src="{{ asset('Admin/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
@@ -27,6 +26,9 @@
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+<script src="/L.Control.Layers.Tree.js"></script>
+@yield('script_peta')
 @yield('script_warna')
 @yield('script_tabel')
 </body>
--- resources/views/admin/peta_kecelakaan.blade.php
+++ resources/views/admin/peta_kecelakaan.blade.php
@@ -1,78 +1,123 @@
 @extends('admin/template')
 @section('content')
-<div id="mapid" style="width: 1050px; height: 600px"></div>
+    <div id="map" style="height:650px; width: 1050px;"></div>
 @stop
 @section('script_peta')
-<script>
-    var street = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
-        maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-    });
+    <script type="text/javascript">
+        var satellite = L.tileLayer('http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3']
+        });
 
-    var hybrid = L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}',{
-        maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-    });
+        var street = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3']
+        });
 
-    var satellite = L.tileLayer('http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}',{
-        maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-    });
+        var hybrid = L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3']
+        });
 
-    var kabupatenJson;
+        var map = L.map('map', {
+            layers: [street, hybrid, satellite],
+            center: [-5.420000, 105.292969],
+            zoom: 12.4
+        });
 
-    $.ajax({
-        url: "/kabupaten.geojson",
-        async: false,
-        dataType: 'json',
-        success: function(data){
-            kabupatenJson = data
-        }
-    });
+        var baseTree = [
+            {
+                label: 'Street',
+                layer: street,
+            },
+            {
+                label: 'Hybrid',
+                layer: hybrid,
+            },
+            {
+                label: 'Satellite',
+                layer: satellite,
+            },
+        ];
 
-    var kabupatenStyle = {
-        "color": '#000000',
-        "weight": 2,
-        "opacity": 1,
-        "fillOpacity": 0 ,
-    };
+        var kabupatenJson;
 
-    var batasKabupaten = L.geoJSON(kabupatenJson, {
-        style: kabupatenStyle
-    });
+        $.ajax({
+            url: "/kabupaten.geojson",
+            async: false,
+            dataType: 'json',
+            success: function(data){
+                kabupatenJson = data
+            }
+        });
 
-    var batasKecamatan = L.layerGroup();
-
-    var myMap = L.map('mapid', {
-        center: [-5.420000, 105.292969],
-        zoom: 12.4,
-        layers: [street, hybrid, satellite, batasKabupaten, batasKecamatan]
-    });
-
-    var baseMaps = {
-        "Street": street,
-        "Hybrid": hybrid,
-        "Satellite": satellite,
-    };
-
-    var overlayMaps = {
-        "Kabupaten": batasKabupaten,
-        "Kecamatan": batasKecamatan
-    };
-
-    @foreach($kecamatans as $kec)
-        var kecamatanStyle = {
-            "color": "<?= $kec->warnaKecamatan ?>",
-            "weight": 0,
+        var kabupatenStyle = {
+            "color": '#000000',
+            "weight": 2,
             "opacity": 1,
-            "fillOpacity": 0.5 ,
+            "fillOpacity": 0 ,
         };
-        L.geoJSON(<?= $kec->batasKecamatan ?>,{
-            style: kecamatanStyle    
-        }).addTo(batasKecamatan).bindPopup(" <?= $kec->namaKecamatan ?> ");
-    @endforeach
-    
-    var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(myMap);  
 
-</script>
+        var overlaysTree = 
+            {
+                label: 'Layers',
+                selectAllCheckbox: 'Un/select all',
+                children: [
+                    {label: '<div id="onlysel">-Show only selected-</div>'},
+                    {
+                        label: 'Kecamatan',
+                        selectAllCheckbox: true,
+                        children: [
+                            @foreach($kecamatans as $kec)
+                                {
+                                    label: '<?= $kec->namaKecamatan ?>', layer: L.geoJSON(<?= $kec->batasKecamatan ?>, {
+                                        style: {
+                                            "color": "<?= $kec->warnaKecamatan ?>",
+                                            "weight": 0,
+                                            "opacity": 1,
+                                            "fillOpacity": 0.5 ,
+                                        },
+                                    })
+                                },
+                            @endforeach
+                        ]
+                    },  {
+                        label: 'Kota',
+                        selectAllCheckbox: true,
+                        children: [
+                            {
+                                label: 'Bandar Lampung',
+                                layer: L.geoJSON(kabupatenJson, {
+                                    style: kabupatenStyle
+                                })
+                            }
+                        ]
+                    }
+                ]
+            };
+
+        var lay = L.control.layers.tree(baseTree, overlaysTree, {
+                    namedToggle: true,
+                    selectorBack: false,
+                    closedSymbol: '&#8862; &#x1f5c0;',
+                    openedSymbol: '&#8863; &#x1f5c1;',
+                    collapsed: false,
+                });
+
+        var makePopups = function(node) {
+            if (node.layer) {
+                node.layer.bindPopup(node.label);
+            }
+            if (node.children) {
+                node.children.forEach(function(element) { makePopups(element); });
+            }
+        };
+        makePopups(overlaysTree);
+
+        lay.addTo(map).collapseTree().expandSelected().collapseTree(true);
+        L.DomEvent.on(L.DomUtil.get('onlysel'), 'click', function() {
+            lay.collapseTree(true).expandSelected(true);
+        });
+
+    </script>
 @stop
